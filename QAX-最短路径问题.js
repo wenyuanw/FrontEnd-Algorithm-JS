8bd26f4c78c8f82580874e6a5ef956d53609eb56
Ran Wang <wenyuan19@163.com>
2021-03-08 09:22:36 +0800

    最短路径弗洛伊德解法

--- QAX-最短路径问题.js
+++ QAX-最短路径问题.js
@@ -2,7 +2,7 @@
  * @Author: Ran
  * @Date: 2021-03-06 17:01:54
  * @LastEditors: Ran
- * @LastEditTime: 2021-03-07 13:57:46
+ * @LastEditTime: 2021-03-07 16:38:10
  * @FilePath: \JZoffer\QAX-最短路径问题.js
  * @Description: 
  * --------20210306奇安信笔试--------
@@ -40,8 +40,12 @@
 // https://blog.csdn.net/u012712087/article/details/77995721?utm_source=blogxgwz3
 // https://www.jianshu.com/p/9f46f4d253d1
 // https://blog.csdn.net/qq_34153210/article/details/104497912
-// 处理输入：
+// 算法的解释：https://zhuanlan.zhihu.com/p/114203860
 
+// 可以尝试用弗洛伊德(Floyd-Warshall) 算法来做
+
+// 解法一：迪杰斯特拉算法
+// 处理输入：
 // 第一行：
 var point_num = parseInt(readline()); // 顶点数量
 
@@ -79,6 +83,7 @@ console.log('起点：' + start_point);
 console.log('终点：' + end_point);
 console.log('邻接矩阵如下:');
 console.log(graph);
+// 无向图： 邻接矩阵是对称的， 若是有向图则不是对称的。
 // graph = [
 //     [0, 2, 3, 0, 5, 0],
 //     [2, 0, 0, 0, 1, 4],
@@ -87,7 +92,6 @@ console.log(graph);
 //     [5, 1, 0, 3, 0, 5],
 //     [0, 4, 0, 4, 5, 0]
 // ];
-
 const INF = Number.MAX_SAFE_INTEGER;
 // 查找最近的点
 const minDistance = (dist, visited) => {
@@ -101,7 +105,6 @@ const minDistance = (dist, visited) => {
     }
     return minIndex;
 };
-
 /** 
  * @param {图：邻接矩阵} graph 
  * @param {顶点索引} src 
@@ -110,20 +113,22 @@ const dijkstra = (graph, src) => {
     const dist = [];
     // 用于标识顶点 src 至其他顶点的距离是否确定
     const visited = [];
-    const {
-        length
-    } = graph;
+    const { length } = graph;
     for (let i = 0; i < length; i++) {
         dist[i] = INF;
         visited[i] = false;
     }
     dist[src] = 0;
     for (let i = 0; i < length - 1; i++) {
+        // 找出最小距离的点
         const u = minDistance(dist, visited);
         // 标识顶点 src 到此顶点的距离已经确认
         visited[u] = true;
         for (let v = 0; v < length; v++) {
-            if (!visited[v] && graph[u][v] !== 0 && dist[u] !== INF && dist[u] + graph[u][v] < dist[v]) {
+            if (!visited[v] &&
+                graph[u][v] !== 0 &&
+                dist[u] !== INF &&
+                dist[u] + graph[u][v] < dist[v]) {
                 // 更新 dist
                 dist[v] = dist[u] + graph[u][v];
             }
@@ -131,19 +136,87 @@ const dijkstra = (graph, src) => {
     }
     return dist;
 };
-
 const dist = dijkstra(graph, start_point); // 返回值保存的是起点到所有顶点的最短距离
 const min_dist = dist[end_point];
 console.log('起点 ' + start_point + ' 到终点 ' + end_point + ' 的最短距离为：\n' + min_dist);
 
-// for (i = 0; i < dist.length; i++) {
-//     console.log('起点SRC到第' + i + '顶点的最短距离：\n' + dist[i]);
-// }
 
-// result
-// 0 0
-// 1 2
-// 2 4
-// 3 6
-// 4 4
-// 5 6
+
+
+// 解法二：迪杰斯特拉算法
+// 参考：https://zhuanlan.zhihu.com/p/114203860
+
+// 处理输入：
+// 第一行：
+var point_num = parseInt(readline()); // 顶点数量
+
+// 第二行：
+var line_one = readline().split(" ");
+var start_point = parseInt(line_one[0]); // 起点
+var end_point = parseInt(line_one[1]); // 终点
+
+// 利用二维数组定义邻接矩阵,point_num * point_num,全部赋初值为负无穷
+// 开始的时候遇到了深拷贝，浅拷贝的问题，对JS的二维数组初始化方式不够熟悉
+var graph = new Array(); //声明一维数组        
+for (var x = 0; x < point_num; x++) {
+    graph[x] = new Array(); //声明二维数组
+    const INF = Number.MAX_SAFE_INTEGER;
+    for (var y = 0; y < point_num; y++) {
+        graph[x][y] = INF; //数组初始化为 正无穷
+    }
+}
+
+// 第三行开始：
+// 生成对应邻接矩阵
+let flag = true;
+while (flag) {
+    let lines = readline().split(" ");
+    for (var j = 0; j < lines.length; j++) {
+        if (lines[0] == '0' && lines[1] == '0' && lines[2] == '0') {
+            flag = false;
+            // 结尾 0 0 0 ，退出循环
+        }
+        graph[lines[0]][lines[1]] = parseInt(lines[2]); // 转为数字存进数组
+        graph[lines[1]][lines[0]] = parseInt(lines[2]);
+    }
+}
+
+// 无向图： 邻接矩阵是对称的， 若是有向图则不是对称的。
+// graph = [
+//     [INF, 2, 3, INF, 5, INF],
+//     [2, INF, INF, INF, 1, 4],
+//     [3, INF, INF, 6, INF, INF],
+//     [INF, 6, INF, INF, 3, 4],
+//     [5, 1, INF, 3, INF, 5],
+//     [INF, 4, INF, 4, 5, INF]
+// ];
+
+const floydWarshall = (graph) => {
+    const dist = [];
+    const { length } = graph;
+
+    // 初始化dist
+    for (let i = 0; i < length; i++) {
+        dist[i] = [];
+        for (let j = 0; j < length; j++) {
+            if (i === j) {
+                dist[i][j] = 0;
+            } else {
+                dist[i][j] = graph[i][j];
+            }
+        }
+    }
+
+    // 核心操作，判断 k 是否是 i 到 j 可能的中点
+    for (let k = 0; k < length; k++) {
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+    return dist;
+}
+console.log(floydWarshall(graph)[start_point][end_point]); // 可以求出任意两个点之间的最短路径
