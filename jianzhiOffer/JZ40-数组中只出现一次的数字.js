d5c2607dfb12ffb226719e9bbe491d2ee2f79bde
wwenyuan <wenyuan19@163.com>
2021-04-02 09:49:27 +0800

    更新面经

--- jianzhiOffer/JZ40-数组中只出现一次的数字.js
+++ jianzhiOffer/JZ40-数组中只出现一次的数字.js
@@ -1,11 +1,13 @@
 /*
  * @Author: Ran
  * @Date: 2021-04-01 08:53:39
- * @LastEditTime: 2021-04-01 09:43:12
+ * @LastEditTime: 2021-04-01 11:47:24
  * @FilePath: \JZoffer\jianzhiOffer\JZ40-数组中只出现一次的数字.js
  * @Description: 
  * 
  */
+
+// 位运算不是很好理解...
 
 // 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
 function FindNumsAppearOnce(array) {
@@ -52,3 +54,48 @@ var singleNumber = function(nums) {
 let array = [3, 4, 3, 3];
 
 console.log(singleNumber(array));
+
+/* 思路1:去重(空间复杂度不能满足)
+通过对象obj记录，key为当前数字，value为任意值。
+遍历时判断obj中是否存在这个key，如果存在就删除，否则就记录
+遍历完成后，obj中仅有的两个key就是最终结果
+(以上可以修改成使用set、map等方式记录数据)
+javascript
+示例代码：
+
+作者：shui-_shui-___
+链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution/qu-zhong-huo-zhe-wei-yun-suan-si-xiang-b-iup2/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。 */
+
+const singleNumbers = (nums) => {
+    const obj = {}
+    nums.forEach(item => {
+        if (obj[item]) {
+            delete obj[item]
+        } else {
+            obj[item] = true
+        }
+    })
+    return Object.keys(obj)
+}
+
+// 因为除了要返回的数都是出现两次，所以set最后只会返回我们要的结果，然后直接return [...set].reverse(),
+// 因为你是两头遍历，所以set保存的顺序要颠倒一下
+var singleNumbers = function(nums) {
+    let set = new Set();
+    let i = 0,
+        j = nums.length - 1;
+    while (i < j) {
+        set.has(nums[i]) ? set.delete(nums[i]) : set.add(nums[i]);
+        set.has(nums[j]) ? set.delete(nums[j]) : set.add(nums[j]);
+        i++;
+        j--;
+    }
+    return [...set].reverse();
+};
+
+// 作者：zzilcc
+// 链接：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution/li-yong-setran-hou-tong-shi-liang-tou-bian-li-zui-/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
