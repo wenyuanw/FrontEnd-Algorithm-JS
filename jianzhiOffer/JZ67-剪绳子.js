fa20d14a44ddc2474f1c939dc84d329cccddcb98
wwenyuan <wenyuan19@163.com>
2021-04-12 20:33:01 +0800

    更新面经

---
+++ jianzhiOffer/JZ67-剪绳子.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Ran
+ * @Date: 2021-04-12 11:09:55
+ * @LastEditTime: 2021-04-12 20:31:25
+ * @FilePath: \JZoffer\jianzhiOffer\JZ67-剪绳子.js
+ * @Description: 
+ * 给你一根长度为n的绳子，请把绳子剪成整数长的m段（m、n都是整数，n>1并且m>1，m<=n），
+ * 每段绳子的长度记为k[1],...,k[m]。请问k[1]x...xk[m]可能的最大乘积是多少？
+ * 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+ */
+
+function cutRope(number) {
+    // write code here
+}
+module.exports = {};
