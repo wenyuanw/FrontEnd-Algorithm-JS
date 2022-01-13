aa68eaf55bcf009a1fb072dd57c7405b0d7bc890
wenyuan <wenyuan19@163.com>
2022-01-13 19:53:26 +0800

    Revert "test Copilot"
    This reverts commit bd611b2331e98323884cc528a1be93bf5c64bdc6.

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Ran
  * @Date: 2021-12-26 22:50:53
- * @LastEditTime: 2021-12-26 22:54:02
- * @FilePath: \FrontEnd\FrontEnd-Algorithm-JS\test.js
+ * @LastEditTime: 2022-01-13 19:53:03
+ * @FilePath: \FrontEnd-Algorithm-JS\test.js
  * @Description: 
  */
 function p() {
@@ -12,23 +12,3 @@ var p1 = new p()
 p1.like.push('3')
 
 console.log(p.prototype.like);
-
-let a = 1
-let b = 2
-let c = 3
-let d = 4
-let e = 5
-    // 防抖函数
-function debounce(fn, delay) {
-    let timer
-    return function() {
-        let context = this
-        let args = arguments
-        clearTimeout(timer)
-        timer = setTimeout(function() {
-            fn.apply(context, args)
-        }, delay)
-    }
-}
-// Copilot牛逼
-// what happened
